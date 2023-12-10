d21c8ca8357f01111cd9339f02eec42fdb2bf408
TomatoTwister <hemantjatal59@gmail.com>
2023-12-10 21:13:16 +0530

    navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,11 +12,15 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setMode("dark");
       document.documentElement.classList.add("dark");
     } else {
-      setMode("dark");
+      setMode("light");
       document.documentElement.classList.remove("dark");
     }
   };
