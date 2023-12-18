faf14dd6a2443a843eb7a7578314cbdd3489f894
TomatoTwister <hemantjatal59@gmail.com>
2023-12-18 10:53:13 +0530

    QuestionCard

--- components/shared/Navbar/MobileNavbar.tsx
+++ components/shared/Navbar/MobileNavbar.tsx
@@ -18,7 +18,7 @@ const MobileNavbar = () => {
     const pathname = usePathname();
 
     return (
-      <section className="flex h-full flex-col gap-6 pt-16">
+      <section className="flex h-full flex-col gap-1 pt-5">
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
@@ -31,7 +31,7 @@ const MobileNavbar = () => {
                   isActive
                     ? "primary-gradient rounded-lg text-light-900 "
                     : "text-dark300_light900"
-                } flex items-center justify-start gap-4 bg-transparent p-4`}
+                } flex items-center justify-start gap-4 bg-transparent p-3 text-sm`}
               >
                 <Image
                   src={item.imgURL}
@@ -40,7 +40,7 @@ const MobileNavbar = () => {
                   alt={item.label}
                   className={`${isActive ? "" : "invert-colors"}`}
                 />{" "}
-                <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                <p className={`${isActive ? "base-bold" : "base-medium"} `}>
                   {item.label}
                 </p>
               </Link>
@@ -73,33 +73,32 @@ const MobileNavbar = () => {
             height={23}
           />
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk ">
-            Dev <span className="text-primary-500">Flow</span>
+            Dev <span className="text-primary-500">Overflow</span>
           </p>
         </Link>
-        <div>
+        <div className=" flex h-full flex-col justify-between py-6">
           <SheetClose className="w-full">
             <NavContent />
           </SheetClose>
-          <SignedOut>
-            <div className="flex flex-col gap-3">
-              <SheetClose>
+          <div>
+            <SignedOut>
+              <div className="flex flex-col gap-3">
                 <Link href={"/sign-in"}>
                   {" "}
                   <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3">
                     <span className="primary-text-gradient">Sign In</span>
                   </Button>
                 </Link>
-              </SheetClose>
-              <SheetClose>
+
                 <Link href={"/sign-up"}>
                   {" "}
                   <Button className="small-medium light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3">
                     <span className="text-dark400_light900">Sign Up</span>
                   </Button>
                 </Link>
-              </SheetClose>
-            </div>
-          </SignedOut>
+              </div>
+            </SignedOut>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
